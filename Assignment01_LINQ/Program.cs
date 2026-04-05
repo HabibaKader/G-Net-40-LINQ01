2b106ca903d992449aa342da8e80cb467f70b983
HabibaKader <habibakader@outlook.com>
2026-04-05 23:23:02 +0200

    Products class

--- Assignment01_LINQ/Program.cs
+++ Assignment01_LINQ/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        #region MyRegion
+        static List<Product> ProductList = new List<Product>
+        {
+            new Product { ProductName = "Chai", Category = "Beverages", UnitPrice = 18, UnitsInStock = 39 },
+            new Product { ProductName = "Chang", Category = "Beverages", UnitPrice = 19, UnitsInStock = 17 },
+            new Product { ProductName = "Aniseed Syrup", Category = "Condiments", UnitPrice = 10, UnitsInStock = 13 },
+            new Product { ProductName = "Chef Anton's Cajun Seasoning", Category = "Condiments", UnitPrice = 22, UnitsInStock = 0 },
+            new Product { ProductName = "Ikura", Category = "Seafood", UnitPrice = 31, UnitsInStock = 20 },
+            new Product { ProductName = "Konbu", Category = "Seafood", UnitPrice = 6, UnitsInStock = 24 },
+            new Product { ProductName = "Tofu", Category = "Produce", UnitPrice = 23, UnitsInStock = 0 },
+            new Product { ProductName = "Pavlova", Category = "Confections", UnitPrice = 17, UnitsInStock = 29 }
+        };
+        #endregion
         static void Main(string[] args)
         {
-            
+
         }
     }
 }
